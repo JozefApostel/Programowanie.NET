3068ac3c375af0df056a4bc81f37b9075204d2ad
Józef Apostel <gdx164465@student.gdansk.merito.pl>
2024-05-14 21:27:40 +0200

    first commit

--- laboratorium1/zadanie4/zadanie4.js
+++ laboratorium1/zadanie4/zadanie4.js
@@ -1,26 +1,22 @@
 const fs = require('fs');
 
-// Nazwa pliku tekstowego do odczytu
 const nazwaPliku = 'tekstowy_plik.txt';
 
-// Otwarcie pliku i wczytanie linii po linii
 fs.readFile(nazwaPliku, 'utf8', (err, data) => {
   if (err) {
     console.error('Wystąpił błąd podczas odczytu pliku:', err);
     return;
   }
 
-  // Podziel wczytane dane na linie
   const lines = data.split('\n');
 
-  // Iteruj przez każdą linię i wyświetl ją odwróconą
+
   lines.forEach(line => {
-    const reversedLine = reverseString(line.trim()); // Odwrócenie linii
+    const reversedLine = reverseString(line.trim()); 
     console.log(reversedLine);
   });
 });
 
-// Funkcja do odwracania stringa
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
