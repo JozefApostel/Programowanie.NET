cdd8a1f929c63f13a638b759d0023f897403c01b
Józef Apostel <gdx164465@student.gdansk.merito.pl>
2024-05-14 21:28:01 +0200

    first commit

--- laboratorium1/zadanie6/zadanie6.js
+++ laboratorium1/zadanie6/zadanie6.js
@@ -1,26 +1,19 @@
 const fs = require('fs');
 
-// Nazwa pliku źródłowego
 const plikZrodlowy = 'plik_zrodlowy.txt';
 
-// Nazwa pliku docelowego
 const plikDocelowy = 'plik_docelowy.txt';
 
-// Utwórz strumień do odczytu danych z pliku źródłowego
 const strumienOdczytu = fs.createReadStream(plikZrodlowy);
 
-// Utwórz strumień do zapisu danych do pliku docelowego
 const strumienZapisu = fs.createWriteStream(plikDocelowy);
 
-// Kopiowanie danych z pliku źródłowego do pliku docelowego
 strumienOdczytu.pipe(strumienZapisu);
 
-// Obsługa zdarzenia zakończenia kopiowania
 strumienOdczytu.on('end', () => {
   console.log('Kopiowanie zakończone.');
 });
 
-// Obsługa zdarzenia błędu
 strumienOdczytu.on('error', (err) => {
   console.error('Wystąpił błąd podczas odczytu pliku źródłowego:', err);
 });
